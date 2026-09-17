fda0e371bdd0b1b91a50c1aeef7e2b41d96347a0
Finik <andrijsvec703@gmail.com>
2026-09-17 11:30:10 +0300

    feat(backend): add new functionality for admins to case controller

--- Back/src/controllers/caseController.js
+++ Back/src/controllers/caseController.js
@@ -1,5 +1,6 @@
-const prisma = require("prisma")
+const prisma = require("../config/db")
 const {getRandomSkin} = require("../utils/randomSkinGeter.js")
+const {error} = require("@mermaid-js/mermaid-cli");
 
 const getCases = async (req, res) => {
     try{
@@ -87,4 +88,119 @@ const openCase = async (req, res) => {
     }catch(error){
         res.status(500).json({ message: "Error while opening case", error: error.message })
     }
+}
+
+//=============================FOR ADMINS=============================
+const createCase = async (req, res) => {
+    try{
+        const { name, price, imageUrl, description, collectionId } = req.body
+
+        if(!name || price === undefined){
+            return res.status(400).json({ message: "Name and price are required" })
+        }
+
+        const newCase = await prisma.create({
+            data: {
+                name: name,
+                price : parseFloat(price),
+                imageUrl : imageUrl,
+                description : description,
+                collectionId : collectionId,
+            }
+        })
+
+        res.status(201).json( { message: "Case was added", case: newCase })
+    } catch(error){
+        res.status(500).json({ message: "Error while creating case", error: error.message })
+    }
+}
+
+const updateCase = async (req, res) => {
+    try{
+        const { id } = req.params
+        const { name, price, imageUrl, description, isActive } = req.body
+
+        const updatedCase = await prisma.case.update({
+            where: { id },
+            data: {
+                ...(name && { name }),
+                ...(price !== undefined && { price: parseFloat(price) }),
+                ...(imageUrl && { imageUrl }),
+                ...(description !== undefined && { description }),
+                ...(isActive !== undefined && { isActive })
+            }
+        })
+
+        res.status(200).json({ message: "Case was updated", case: updatedCase })
+    } catch(error){
+        res.status(500).json({ message: "Error while updating case", error: error.message })
+    }
+}
+
+const deleteCase = async (req, res) => {
+    try{
+        const {id} = req.params
+
+        await prisma.$transaction([
+            prisma.caseSkins.deleteMany({
+                where: { caseId: id }
+            }),
+            prisma.case.delete({
+                where: { id }
+            })
+        ])
+
+        res.status(200).json({ message: "Case was deleted"})
+    } catch(error){
+        res.status(500).json({ message: "Error while deleting case", error: error.message })
+    }
+}
+
+const addSkinsToCase = async (req, res) => {
+    try{
+        const {id: caseId} = req.params
+        const {skins} = req.body
+
+        if (skins.length === 0) {
+            return res.status(400).json({message: "No skins in case"})
+        }
+
+        let totalChance = 0
+        skins.forEach((skin) => {
+            totalChance += Number(skin.dropChance)
+        })
+
+        if (Math.abs(totalChance - 100) > 0.1) {
+            return res.status(400).json({message: `Total drop chance must = 100, curren total drop chance = ${totalChance}`})
+        }
+
+        await prisma.$transaction(async (tx) => {
+            await tx.caseSkins.deleteMany({
+                where: {caseId}
+            })
+
+            await tx.caseSkins.createMany({
+                data: skins.map((cs) => ({
+                    caseId,
+                    skinId: cs.skinId,
+                    dropChance: parseFloat(cs.dropChance),
+                }))
+            })
+        })
+
+        res.status(200).json({ message: "Skins were added successfully" })
+    } catch(error){
+        res.status(500).json({ message: "Error while adding skins to case", error: error.message })
+    }
+}
+
+
+module.exports = {
+    getCases,
+    getCaseById,
+    openCase,
+    createCase,
+    updateCase,
+    deleteCase,
+    addSkinsToCase
 }
