a82b06021c970630294b571398bb5c103c73b5f9
Finik <andrijsvec703@gmail.com>
2026-09-17 12:02:51 +0300

    feat(backend): add new routes for admin operations

--- Back/src/routes/caseRoutes.js
+++ Back/src/routes/caseRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const { getCases, getCaseById, openCaseController } = require("../controllers/caseController.js")
+const { getCases, getCaseById, openCase, createCase, updateCase, deleteCase, addSkinsToCase } = require("../controllers/caseController.js")
 const authMiddleware = require("../middleware/authMiddleware.js")
+const adminMiddleware = require("../middleware/adminMiddleware.js")
 
 router.get("/", getCases)
 router.get("/:id", getCaseById)
-router.put("/:id/open", authMiddleware, openCaseController)
+router.post("/:id/open", authMiddleware, openCase)
+router.post("/", authMiddleware, adminMiddleware, createCase)
+router.put("/:id", authMiddleware, adminMiddleware, updateCase)
+router.delete("/:id", authMiddleware, adminMiddleware, deleteCase)
+router.post("/:id/skins", authMiddleware, adminMiddleware, addSkinsToCase)
 
 module.exports = router
 
