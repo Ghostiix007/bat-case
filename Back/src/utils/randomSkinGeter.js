c52c470d8359956d4ede73f2d868ebfc9d40b5c8
Finik <andrijsvec703@gmail.com>
2026-09-17 23:49:10 +0300

    feat(backend): add new functionality to random skin getter util

--- Back/src/utils/randomSkinGeter.js
+++ Back/src/utils/randomSkinGeter.js
@@ -1,17 +1,48 @@
-function getRandomSkin(skins){
+const RARITY_WEIGHTS = {
+    consumer: 72,
+    industrial: 56,
+    milspec: 44,
+    restricted: 22,
+    classified: 10,
+    covert: 3.4,
+    mythic: 0.85
+}
+
+function calculateSkinWeight(skinPrice, casePrice, rarity){
+    const rarityWeight = RARITY_WEIGHTS[rarity?.toLowerCase()]
+    const ratio = skinPrice > 0 ? casePrice / skinPrice : 1
+    const valuePenalty = Math.min(Math.max(ratio, 0.12), 1)
+
+    let premiumPenalty = 1
+    if(casePrice >= 800)
+        premiumPenalty = 0.42
+    else if(casePrice >= 450)
+        premiumPenalty = 0.58
+    else if(casePrice >= 250)
+        premiumPenalty = 0.776
+
+    return rarityWeight * valuePenalty * premiumPenalty
+}
+
+function getRandomSkin(caseSkins, casePrice){
+    const preparedSkins = caseSkins.map((item) => {
+        const chance = calculateSkinWeight(Number(item.skin.price), Number(casePrice), item.skin.rarity)
+        return { ...item.skin, dropChance: chance }
+    })
+
     let totalChance = 0
 
-    skins.forEach(skin => {
-        totalChance += skin.drop_chance
+    preparedSkins.forEach(skin => {
+        totalChance += Number(skin.drop_chance)
     })
 
     let rand = Math.random() * totalChance
 
-    for(const skin of skins){
-        if(rand < skin.drop_chance){
+    for(const skin of preparedSkins){
+        if(rand < Number(skin.drop_chance)){
             return skin
         }
-        rand -= skin.drop_chance
+        rand -= Number(skin.drop_chance)
     }
 }
 
