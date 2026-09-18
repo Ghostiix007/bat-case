420dc13cd4371b5e30d14489cc4514abb421ba15
Finik <andrijsvec703@gmail.com>
2026-09-18 22:47:16 +0300

    feat(backend): add deposit api and connect it to the server

--- Back/server.js
+++ Back/server.js
@@ -13,6 +13,7 @@ const skinRoutes = require('./src/routes/skinRoutes.js')
 const inventoryRoutes = require('./src/routes/inventoryRoutes.js')
 const userRoutes = require('./src/routes/userRoutes.js')
 const upgradeRoutes = require('./src/routes/upgradeRoutes.js')
+const depositRoutes = require('./src/routes/depositRoutes.js')
 
 const PORT = config.PORT
 
@@ -29,6 +30,7 @@ app.use("/api/inventory", inventoryRoutes)
 app.use("/api/user", userRoutes)
 app.use("api/skins", skinRoutes)
 app.use("api/upgrade", upgradeRoutes)
+app.use("api/deposit", depositRoutes)
 
 app.listen(PORT, () => {
     console.log(`BatCase Server running on port ${PORT}\nOn address http://localhost:${PORT}`)
